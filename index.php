e59651524624b41100cf6f79aa2688fb18838601
Shyam Sulbhewar <56698436+shyamwin@users.noreply.github.com>
2020-06-16 13:54:06 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<img src="d5pfgjjqih6c8.cloudfront.net" >
+<h1> This is my webiste from AWS And The Image is comming from CloudFront service </h1>
+<h3>
+<?php>
+print 'ifconfig'
+
+</php>
+</h3>
+<h1 style="color : red">
+<?php>
+print 'ifconfig | grep IP'
+</php>
+</h1>
+</html>
+
